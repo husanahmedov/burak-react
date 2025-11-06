0e737f4eb88b6d875fcda0f0f86e338984049c52
husanahmedov <mr.southkoreakhusan@gmail.com>
2025-11-07 02:13:57 +0900

    MIU customized integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,24 @@
 import React from "react";
-// @ts-ignore
-import "../css/App.css";
+import "../css/index.css";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { RippleBadge } from "./MaterialTheme/styled";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <Container maxWidth="sm">
+      <Stack flexDirection={"column"}>
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component={"h4"}>
+            Create React App On TYpeScript with REDUX
+          </Typography>
+        </Box>
+        <Box>
+          <RippleBadge badgeContent={4}></RippleBadge>
+        </Box>
+        <Button variant="contained">Contained</Button>
+      </Stack>
+    </Container>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
-// @ts-ignore
-import "./css/index.css";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./app/MaterialTheme";
+import { createRoot } from "react-dom/client";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -13,7 +15,10 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
