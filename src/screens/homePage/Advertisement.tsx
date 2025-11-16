c9dd73ffa2a7129b1be45c66a37248279b5eb67a
husanahmedov <mr.southkoreakhusan@gmail.com>
2025-11-16 15:47:15 +0900

    feat: develop advertisement sectional components

--- src/screens/homePage/Advertisement.tsx
+++ src/screens/homePage/Advertisement.tsx
@@ -1,9 +1,16 @@
-import { Container } from "@mui/material";
-
 export default function Advertisement() {
   return (
-    <div>
-      <Container> Advertisement </Container>
+    <div className="ads-restaurant-frame">
+      <video
+        className="ads-video"
+        autoPlay={true}
+        loop
+        muted
+        playsInline
+        data-video-media=""
+      >
+        <source type="video/mp4" src="video/burak-ads.mp4" />
+      </video>
     </div>
   );
 }
