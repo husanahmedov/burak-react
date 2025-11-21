349510503f7d2615dad9ef5160c8460432a38249
husanahmedov <mr.southkoreakhusan@gmail.com>
2025-11-21 18:10:34 +0900

    feat: publish helpPage and userPage screen component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,7 @@ import { Route, Switch, useLocation } from "react-router-dom";
 import OrdersPage from "../screens/ordersPage";
 import { HomeNavbar } from "../components/headers/HomeNavbar";
 import ProductsPage  from "../screens/productsPage";
-import { UsersPage } from "../screens/userPage";
+import UsersPage from "../screens/userPage";
 import HomePage from "../screens/homePage";
 import { OtherNavbar } from "../components/headers/OtherNavbar";
 import Footer from "../components/footers";
@@ -11,7 +11,7 @@ import Footer from "../components/footers";
 import "../css/App.css";
 import "../css/Navbar.css";
 import "../css/footer.css";
-import { HelpPage } from "../screens/helpPage";
+import  HelpPage  from "../screens/helpPage";
 
 function App() {
   const location = useLocation();
