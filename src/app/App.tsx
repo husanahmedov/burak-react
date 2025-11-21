7e5b5940f436634f0e0fe20c19d9b68b5b9f20a8
husanahmedov <mr.southkoreakhusan@gmail.com>
2025-11-21 18:06:14 +0900

    feat: publish orderPage screen component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch, useLocation } from "react-router-dom";
-import { OrdersPage } from "../screens/ordersPage";
+import OrdersPage from "../screens/ordersPage";
 import { HomeNavbar } from "../components/headers/HomeNavbar";
 import ProductsPage  from "../screens/productsPage";
 import { UsersPage } from "../screens/userPage";
