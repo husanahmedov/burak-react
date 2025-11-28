c665ce218202eafcc78342afede5a0784357fb3e
husanahmedov <mr.southkoreakhusan@gmail.com>
2025-11-29 00:26:36 +0900

    feat: develop AppRootState and HomePage type intgrations

---
+++ src/libs/types/enums/member.enum.ts
@@ -0,0 +1,14 @@
+/**
+ * Enums
+ *  **/
+
+export enum MemberType {
+  USER = "USER",
+  RESTAURANT = "RESTAURANT",
+}
+
+export enum MemberStatus {
+  ACTIVE = "ACTIVE",
+  BLOCK = "BLOCK",
+  DELETE = "DELETE",
+}
